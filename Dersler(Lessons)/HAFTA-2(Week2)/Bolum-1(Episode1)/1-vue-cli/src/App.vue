9a11d2a9e4d60551505980f848b5a40de9140af9
Elif Seyhan <elfsyhan@hotmail.com>
2021-08-06 10:01:45 +0300

    Week2-Episode1 Lessons are added. 2-todo-app uploaded before using components

---
+++ Dersler(Lessons)/HAFTA-2(Week2)/Bolum-1(Episode1)/1-vue-cli/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <app-header />
+  <div class="container">
+    <h3>Bu benim ilk Vue CLI Uygulamam</h3>
+    <p>Bugün component ve Vue CLI konularını görüyoruz...</p>
+  </div>
+  <counter-item></counter-item>
+</template>
+
+<script>
+import CounterItem from "@/components/CounterItem";
+export default {
+  components: { CounterItem },
+}
+</script>
---
+++ Dersler(Lessons)/HAFTA-2(Week2)/Bolum-1(Episode1)/2-todo-app/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <h3 class="text-center">Todo App</h3>
+    <hr class="my-2" />
+    <label for="todoText"></label>
+    <input @keydown.enter="addnewTodo" type="text" id="todoText" placeholder="Bir şeyler yazınız...">
+    <ul>
+      <li 
+        v-for="todoItem in todoList"
+        :key="todoItem.id"
+        class="d-flex justify-content-between align-items-center">
+        <span>{{ todoItem.text }}</span>
+        <button @click="deleteItem(todoItem)" class="sm red">Sil</button>
+      </li>
+    </ul>
+    <small class="d-flex justify-content-end mt-2 green">{{ todoList.length }} Adet todo vardır.</small>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      todoList: [
+        {id: 1, text: "Bootcamp# 2"},
+        {id: 2, text: "Bootcamp# 2.1"},
+        {id: 3, text: "Bootcamp# 2.2"},
+        {id: 4, text: "Bootcamp# 2.3"},
+        {id: 5, text: "Bootcamp# 2.4"},
+        {id: 6, text: "Bootcamp# 2.5"},
+      ]
+    }
+  },
+  methods: {
+    deleteItem(todoItem){
+      this.todoList = this.todoList.filter(t => t !== todoItem);
+    },
+    addnewTodo(event){
+      this.todoList.push({
+        id: new Date().getTime(),
+        text: event.target.value
+      })
+      event.target.value = ''
+    }
+  },
+}
+</script>
