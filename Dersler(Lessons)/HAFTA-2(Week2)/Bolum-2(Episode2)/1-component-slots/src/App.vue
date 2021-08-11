00b89e735f330c9f2b6da2d2716077dd828e4c5f
Elif Seyhan <elfsyhan@hotmail.com>
2021-08-11 16:49:03 +0300

    Week2-Episode2 1-components-slots

---
+++ Dersler(Lessons)/HAFTA-2(Week2)/Bolum-2(Episode2)/1-component-slots/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Modal>
+    <template #title>
+      <h3>Slot ile Gelen Title Bilgisi</h3>
+    </template>
+    <template #content>
+      <p class="text-green">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi illo et atque provident illum aliquid mollitia perspiciatis, eveniet quisquam eaque in eligendi amet quas maiores accusantium exercitationem veniam libero voluptas.</p>
+    </template>
+    <template #default>
+      Bu default olarak gösterilecek...
+    </template>
+  </Modal>
+</template>
+
+<script>
+import Modal from "@/components/Modal";
+export default {
+  components: {
+    Modal
+  },
+  methods: {
+    onSave(){
+      alert("Saved!!")
+    }
+  }
+}
+</script>
---
+++ Dersler(Lessons)/HAFTA-2(Week2)/Bolum-2(Episode2)/1-component-slots/src/components/Modal.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="header">
+            <slot name="title"></slot>
+        </div>
+        <hr/>
+        <div class="content my-2">
+            <slot name="content"></slot>
+        </div>
+        <slot></slot>
+        <hr/>
+        <div class="footer text-right">
+            <button class="mr-2">Kapat</button>
+            <button class="ml-2 green">Kaydet</button>
+        </div>
+    </div>
+</template>
